27aeb576a3461b4786c5b188c93c94e1a51e2e6a
KarinAlZoabi <karinalzoabi15@gmail.com>
2024-11-07 19:55:21 +0200

    almost done quotes part, not responsive

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   let bestsellers = [];
   let booksDisplay = $("#booksDisplay");
-  let bookC=$(".bookCard")[0]
-  let numberOfDisplayedBooks=Math.floor(booksDisplay.width()/190);
+  let bookC = $(".bookCard")[0]
+  let numberOfDisplayedBooks = Math.floor(booksDisplay.width() / 190);
   let startBooks = 0;
-  let endBooks = numberOfDisplayedBooks-1;
+  let endBooks = numberOfDisplayedBooks - 1;
 
   $.ajax({
     url: "data.json",
@@ -14,7 +14,7 @@ $(document).ready(function () {
       getBestsellers(data);
       displayBestSellingBooks(bestsellers);
     },
-    error: function (error) {},
+    error: function (error) { },
   });
   // gets all the bestsellers found in the data.json file
   function getBestsellers(data) {
@@ -30,7 +30,7 @@ $(document).ready(function () {
     touch: true
   })
 
-  
+
   // creates the carousel items which will be display when a user presses the prev or next btn
   function displayBestSellingBooks(data) {
     let numOfSections = Math.ceil(bestsellers.length / numberOfDisplayedBooks);
@@ -39,7 +39,7 @@ $(document).ready(function () {
       let section = $("<div>");
       section.addClass("bookSection");
       section.addClass("carousel-item");
-      if(j==0){
+      if (j == 0) {
         section.addClass('active')
       }
       for (let i = startBooks; i <= endBooks; i++) {
@@ -55,8 +55,8 @@ $(document).ready(function () {
         bookDiv.append(author);
         let innerDiv = $("<div>").html(
           "$" +
-            data[i].price +
-            " <div> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg> <button class='homePageBtn pinkAddBtn'>Add</button></div>"
+          data[i].price +
+          " <div> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg> <button class='homePageBtn pinkAddBtn'>Add</button></div>"
         );
         innerDiv.addClass("innerBookDiv");
         bookDiv.append(innerDiv);
@@ -70,6 +70,56 @@ $(document).ready(function () {
 });
 
 // This will load the content of header.html into the #navbar div
-$(document).ready(function() {
+$(document).ready(function () {
   $("#header").load("Header/header.html");
+});
+
+//code for the generation of a quote
+$(document).ready(function () {
+  let quotes = [];
+
+  // Fetch quotes from data.json
+  $.getJSON('data.json', function (data) {
+    quotes = data.quotes;
+    loadQuote();
+  });
+
+  // Function to pick a random quote from the quotes array
+  const getRandomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
+
+  const displayQuote = (quote) => {
+    $('#cookie-structure p').first().text(quote.content);
+    // $('#quoteDetails p').first().text(`- ${quote.author}`);
+    // $('#quoteDetails p').last().text(quote.book);
+    $('#quoteDetails p').first().text(`_${quote.author}, ${quote.book}`);
+  };
+
+  const loadQuote = () => {
+    let storedQuote = JSON.parse(localStorage.getItem('dailyQuote'));
+    let storedDate = localStorage.getItem('quoteDate'); // The date when the quote was last updated
+
+    //function to check if 24 hours have passed since the stored date
+    const isNewDay = () => {
+      if (!storedDate) return true;
+      let lastDate = new Date(storedDate);
+      let currentDate = new Date();
+      return (currentDate - lastDate) > (24 * 60 * 60 * 1000);
+    };
+
+    if (!storedQuote || isNewDay()) {
+      let newQuote = getRandomQuote();
+      displayQuote(newQuote);
+      localStorage.setItem('dailyQuote', JSON.stringify(newQuote));
+      localStorage.setItem('quoteDate', new Date().toISOString());
+    } else {
+      displayQuote(storedQuote);
+    }
+  };
+
+  // Button to get another random quote
+  $('#generateQuoteBtn').click(() => {
+    console.log("Button clicked!");  // Debugging line
+    let newQuote = getRandomQuote();
+    displayQuote(newQuote);
+  });
 });
