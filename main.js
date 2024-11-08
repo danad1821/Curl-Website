33d069ec3cb67b40fcaa0b2ad07faa7c7d9344ec
KarinAlZoabi <karinalzoabi15@gmail.com>
2024-11-08 21:30:20 +0200

    finalized community, started working on menu scrollable

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
       getBestsellers(data);
       displayBestSellingBooks(bestsellers);
     },
-    error: function (error) {},
+    error: function (error) { },
   });
   // gets all the bestsellers found in the data.json file
   function getBestsellers(data) {
@@ -53,8 +53,8 @@ $(document).ready(function () {
         bookDiv.append(author);
         let innerDiv = $("<div>").html(
           "$" +
-            data[i].price +
-            " <div> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg> <button class='homePageBtn pinkAddBtn'>Add</button></div>"
+          data[i].price +
+          " <div> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg> <button class='homePageBtn pinkAddBtn'>Add</button></div>"
         );
         innerDiv.addClass("innerBookDiv");
         bookDiv.append(innerDiv);
@@ -120,3 +120,84 @@ $(document).ready(function () {
     displayQuote(newQuote);
   });
 });
+
+
+
+//DISPLAY MENU
+// $(document).ready(function () {
+//   $.getJSON("data.json", function (menuItems) {
+//     // Check if menuItems is an array
+//     if (!Array.isArray(menuItems)) {
+//       console.error("Expected an array of menu items.");
+//       return;
+//     }
+
+//     const menuContainer = $("#menu-items");
+
+//     menuItems.forEach(function (item) {
+//       const menuItem = createMenuItem(item);
+//       menuContainer.append(menuItem);
+//     });
+//   }).fail(function () {
+//     console.error("Error loading menu items.");
+//   });
+// });
+
+// function createMenuItem(item) {
+//   const itemDiv = $("<div>").addClass("menu-item");
+
+//   const img = $("<img>").attr("src", item.img).attr("alt", item.name);
+//   const name = $("<h3>").text(item.name);
+//   const description = $("<p>").text(item.description);
+//   const price = $("<p>").addClass("price").text(`$${item.price}`);
+//   const addButton = $("<button>").addClass("add-btn").text("Add");
+
+//   itemDiv.append(img, name, description, price, addButton);
+//   return itemDiv;
+// }
+
+
+$(document).ready(function () {
+  $.getJSON("data.json", function (data) {
+    // Access the "menu" array inside the JSON data
+    const menuItems = data.menu;
+
+    // Check if menuItems is an array
+    if (!Array.isArray(menuItems)) {
+      console.error("Expected an array of menu items.");
+      return;
+    }
+
+    const menuContainer = $("#menu-items");
+
+    // Limit to the first 10 items
+    const firstTenItems = menuItems.slice(0, 10);
+
+    firstTenItems.forEach(function (item) {
+      const menuItem = createMenuItem(item);
+      menuContainer.append(menuItem);
+    });
+  }).fail(function () {
+    console.error("Error loading menu items.");
+  });
+});
+
+function createMenuItem(item) {
+  const itemDiv = $("<div>").addClass("menu-item");
+  const itemInfo = $("<div>").addClass("menu-info");
+  const itemName = $("<div>").addClass("item-Name");
+  const itemPrice = $("<div>").addClass("item-price");
+
+  const img = $("<img>").attr("src", item.img).attr("alt", item.name);
+  const name = $("<h3>").text(item.name);
+  const description = $("<p>").text(item.description);
+  const price = $("<p>").addClass("price").text(`$${item.price}`);
+  const addButton = $("<button>").addClass("add-btn").text("Add");
+
+  itemName.append(name, description)
+  itemPrice.append(price, addButton)
+  itemInfo.append(itemName, itemPrice)
+  itemDiv.append(img, itemInfo);
+  return itemDiv;
+}
+
