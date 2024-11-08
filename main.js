46502671aee6b283cecccc57b51ae91e762ad47d
danad1821 <danadabdoub@gmail.com>
2024-11-09 00:18:06 +0200

    adds bestselling books to the wishlist

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
-  localStorage.setItem('cart', JSON.stringify({books:[], menuItems:[], merch:[]}));
-  localStorage.setItem('wishlist', JSON.stringify({books:[], menuItems:[], merch:[]}));
+  localStorage.setItem(
+    "cart",
+    JSON.stringify({ books: [], menuItems: [], merch: [] })
+  );
+  localStorage.setItem(
+    "wishlist",
+    JSON.stringify({ books: [], menuItems: [], merch: [] })
+  );
   let bestsellers = [];
   let booksDisplay = $("#booksDisplay");
   let bookC = $(".bookCard")[0];
   let numberOfDisplayedBooks = Math.floor(booksDisplay.width() / 200);
   let startBooks = 0;
   let endBooks = Math.min(numberOfDisplayedBooks - 1, 3);
+  //gets data from json file
   $.ajax({
     url: "data.json",
     type: "GET",
@@ -15,7 +22,7 @@ $(document).ready(function () {
       getBestsellers(data);
       displayBestSellingBooks(bestsellers);
     },
-    error: function (error) { },
+    error: function (error) {},
   });
   // gets all the bestsellers found in the data.json file
   function getBestsellers(data) {
@@ -46,17 +53,17 @@ $(document).ready(function () {
         let bookDiv = $("<div>");
         bookDiv.addClass("bookCard");
         let bookImg = $("<img>");
-        bookImg.prop("src", data[i].img);
+        bookImg.prop("src", data[i]?.img);
         bookImg.addClass("bookImg");
         bookDiv.append(bookImg);
-        let title = $("<b>").text(data[i].title);
-        let author = $("<p>").html("Author: <b>" + data[i].author + "</b>");
+        let title = $("<b>").text(data[i]?.title);
+        let author = $("<p>").html("Author: <b>" + data[i]?.author + "</b>");
         bookDiv.append(title);
         bookDiv.append(author);
         let innerDiv = $("<div>").html(
           "$" +
-          data[i].price +
-          " <div> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg> <button class='homePageBtn pinkAddBtn'>Add</button></div>"
+            data[i]?.price +
+            " <div> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon bookHeartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg> <button class='homePageBtn pinkAddBtn'>Add</button></div>"
         );
         innerDiv.addClass("innerBookDiv");
         bookDiv.append(innerDiv);
@@ -66,8 +73,48 @@ $(document).ready(function () {
       endBooks = endBooks + numberOfDisplayedBooks;
       booksDisplay.append(section);
     }
+
+    // make buttons functional
+    // Select all heart icons
+    const bookHeartIcons = $(".bookHeartIcon");
+
+    bookHeartIcons.click(function () {
+      // Get the current wishlist data from localStorage
+      let wishlist = JSON.parse(localStorage.getItem("wishlist")) || {};
+      // Get the index of the clicked book in the bestsellers array
+      const clickedIndex = $(this).parent().parent().parent().index();
+      //get selected book
+      const selectedBook = bestsellers[clickedIndex];
+      if ($(this).css("fill") !== "rgb(255, 0, 0)") {
+        // Check if the index is valid (within the bestsellers array)
+        if (clickedIndex >= 0 && clickedIndex < bestsellers.length) {
+          // Add the selected book directly to the wishlist array
+          if (!wishlist.books.includes(selectedBook)) {
+            wishlist.books.push(selectedBook);
+          }
+          // Changes the icon color
+          $(this).css({ fill: "rgb(255, 0, 0)" });
+        } else {
+          console.error("Invalid book index!");
+        }
+      } else {
+        // Filter wishlist to remove the selected book
+        wishlist.books = wishlist.books.filter(
+          (book) => book.id !== selectedBook.id
+        );
+        // Changes the icon color
+        $(this).css({ fill: "#37474f" });
+      }
+      // Updates the wishlist data in localStorage
+      localStorage.setItem("wishlist", JSON.stringify(wishlist));
+      console.log(localStorage.getItem("wishlist"));
+    });
+
+    let bestsellerAddBtn = $(".pinkAddBtn");
+    bestsellerAddBtn.click(() => {
+      console.log("hello");
+    });
   }
-  $('.heartIcon')
 
   // This will load the content of header.html into the #navbar div
 
@@ -124,8 +171,6 @@ $(document).ready(function () {
   });
 });
 
-
-
 //DISPLAY MENU
 // $(document).ready(function () {
 //   $.getJSON("data.json", function (menuItems) {
@@ -158,7 +203,6 @@ $(document).ready(function () {
 //   itemDiv.append(img, name, description, price, addButton);
 //   return itemDiv;
 // }
-
 
 $(document).ready(function () {
   $.getJSON("data.json", function (data) {
@@ -197,10 +241,9 @@ function createMenuItem(item) {
   const price = $("<p>").addClass("price").text(`$${item.price}`);
   const addButton = $("<button>").addClass("add-btn").text("Add");
 
-  itemName.append(name, description)
-  itemPrice.append(price, addButton)
-  itemInfo.append(itemName, itemPrice)
+  itemName.append(name, description);
+  itemPrice.append(price, addButton);
+  itemInfo.append(itemName, itemPrice);
   itemDiv.append(img, itemInfo);
   return itemDiv;
 }
-
