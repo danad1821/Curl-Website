5a313048053e5114889a90743aafd556dccfde99
danad1821 <danadabdoub@gmail.com>
2024-11-25 19:49:02 +0200

    user profile section is starting to come together

--- profile/profile.js
+++ profile/profile.js
@@ -6,4 +6,20 @@ $(document).ready(function () {
     $("#signInBtn").show();
     $(location).prop("href", "../index.html");
   });
+  let currentUser=JSON.parse(sessionStorage.getItem("loggedInUser"));
+  $("#userImg").attr("src", currentUser.profilePicture);
+  $("#fullName").text(currentUser.firstName+" "+currentUser.lastName);
+  $("#location").text(currentUser.address);
+  $("#wishlistItems").text(currentUser.wishlist.length+" items");
+  for(const room of currentUser.rooms){
+    let roomBtn=$("<button>").text(room.roomName).addClass("roomBtn btn");
+    $("#rooms").append(roomBtn)
+    roomBtn.click(function () {
+      localStorage.setItem("room", JSON.stringify(room))
+      $(location).prop('href', '../community/room.html')
+    });
+  }
+  for(const event of currentUser.events){
+    let eventDiv=$("<div>")
+  }
 });
