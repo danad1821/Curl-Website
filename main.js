9eee5ee9731abace250e3f5e0972ff0780648d83
danad1821 <danadabdoub@gmail.com>
2024-12-08 18:24:13 +0200

    small issue with bestselling books fixed

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ $(document).ready(function () {
     let wishlist = JSON.parse(localStorage.getItem("wishData")) || {};
 
     $(".bookHeartIcon").each(function () {
-      const clickedId = $(this).parent().parent().parent().parent().data("id");
+      const clickedId = $(this).data("id");
       const heart = $(this);
       const likeIcon = $(this).siblings(".iconImg");
 
@@ -163,7 +163,7 @@ $(document).ready(function () {
           "$" +
           data[i]?.price +
           " <div>" +
-          " <div class='heart-div'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon bookHeartIcon'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg><img class='icon iconImg' src='../designImages/likedHeart.png'/></div>" +
+          " <div class='heart-div'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' class='heartIcon bookHeartIcon' data-id='" + data[i].id + "'><path d='M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z'/></svg><img data-id='" + data[i].id + "' class='icon iconImg' src='../designImages/likedHeart.png'/></div>" +
           "<button class='homePageBtn pinkAddBtn addBestseller' data-id='" + data[i].id + "'>Add</button></div>"
         );
         innerDiv.addClass("innerBookDiv");
@@ -179,7 +179,7 @@ $(document).ready(function () {
       // Get the current wishlist data from localStorage
       let wishlist = JSON.parse(localStorage.getItem("wishData")) || {};
       // Get the id of the clicked book
-      const clickedId = $(this).parent().parent().parent().parent().data("id");
+      const clickedId = $(this).data("id");
       let likeIcon = $(this).siblings();
       let heart = $(this);
 
@@ -194,7 +194,7 @@ $(document).ready(function () {
     $(".iconImg").click(function () {
       // Get the current wishlist data from localStorage
       let wishlist = JSON.parse(localStorage.getItem("wishData")) || {};
-      const clickedId = $(this).parent().parent().parent().parent().data("id");
+      const clickedId = $(this).data("id");
       let likeIcon = $(this);
       let heart = $(this).siblings();
       if (heart.css("display") == "none") {
@@ -203,7 +203,7 @@ $(document).ready(function () {
         heart.css("display", "block");
         likeIcon.css("display", "none");
       }
-      localStorage.setItem("wishlist", JSON.stringify(wishlist));
+      localStorage.setItem("wishData", JSON.stringify(wishlist));
     });
 
     let bestsellerAddBtn = $(".pinkAddBtn");
